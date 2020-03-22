eb4f57fa5bd614482b8f85cc0e05d1d204b5e421
Guillem <guillem.jardi@e-campus.uab.cat>
2020-03-22 14:40:12 +0100

    Service Emergincia Send avis acabat

--- salut/WebApiSalut/Services/emergenciesService.js
+++ salut/WebApiSalut/Services/emergenciesService.js
@@ -18,18 +18,51 @@ class EmergenciesService{
         });
         this.emailService = new emailService();
     }
-    sendAvis(coordenades, callback){
-        console.log(coordenades);
+    sendAvis(req, callback){
+        var self = this;
+        var response = null;
+        console.log(req);
 
-        var Text = "Shan enviat la teva ubicació (latitud: " + coordenades.latitud + " longitut: " + coordenades.longitut + ") correctament.\n";
-        var Text = Text + "Google Maps: https://www.google.com/maps/@" + coordenades.latitud + "," + coordenades.longitut + ",17z"
-        this.emailService.sendEmail('seguisalut@protonmail.com',"Avis rebut", Text,callback)
-        
-        var response = {
-            serverStatus: 200,
-            msg: 'correu enviat'
-        };
-        callback(response)              
+        var Text = "Shan enviat la teva ubicació (latitud: " + req.coordenades.latitud + " longitut: " + req.coordenades.longitut + ") correctament.\n";
+        var Text = Text + "Google Maps: https://www.google.com/maps/@" + req.coordenades.latitud + "," + req.coordenades.longitut + ",17z"
+
+        var query = 'SELECT Correu FROM Usuari WHERE Id =' + req.UsuariId;
+
+        this.connection.connect(function(err){
+            var response;
+            console.log("connected")
+            if (!err){
+                console.log("connected no error")
+                self.connection.query(query, function(error, rows, fields){
+                    if (error){
+                        response = {
+                            serverStatus: 400,
+                            correuEnviat: false,
+                            msg: 'error connection'
+                        };
+                        callback(response);
+                        return; 
+                    }
+                    if (rows.length === 1){
+                        self.emailService.sendEmail(rows[0].Correu,"Avis rebut", Text,callback)
+                    }else{
+                        response = {
+                            serverStatus: 400,   
+                            correuEnviat: false,
+                            msg: 'User invalid'
+                        };
+                        callback(response);
+                    }
+                });
+            }else{
+                response = {
+                        serverStatus: 400, 
+                        correuEnviat: false,
+                        msg: 'error connection'
+                }; 
+                callback(response);
+            }
+        });          
     }
     sendEmergencia(coordenades, callback){
         console.log(coordenades);
