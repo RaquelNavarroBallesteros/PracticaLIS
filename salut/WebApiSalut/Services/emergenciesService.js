54b66e7b2176001f900345dfd4b2954e94c8305c
Guillem <guillem.jardi@e-campus.uab.cat>
2020-03-26 18:54:59 +0100

    Metode enviament d'emergencia al service

--- salut/WebApiSalut/Services/emergenciesService.js
+++ salut/WebApiSalut/Services/emergenciesService.js
@@ -64,13 +64,53 @@ class EmergenciesService{
             }
         });          
     }
-    sendEmergencia(coordenades, callback){
-        console.log(coordenades);
-        var response = {
-            serverStatus: 200,
-            msg: 'correu enviat'
-        };
-        callback(response)              
+    sendEmergencia(req, callback){
+        var self = this;
+        var perfilInformation = JSON.stringify(req.perfil)
+
+        console.log(req);
+
+        var Text = "Shan enviat la teva ubicació (latitud: " + req.coordenades.latitud + " longitut: " + req.coordenades.longitut + ") correctament.\n";
+        Text = Text + "Google Maps: https://www.google.com/maps/@" + req.coordenades.latitud + "," + req.coordenades.longitut + ",17z"
+        Text = Text + "Junatment amb les següents dades: " + perfilInformation
+
+        var query = 'SELECT Correu FROM Usuari WHERE Id =' + req.perfil.UsuariId;
+
+        this.connection.connect(function(err){
+            var response;
+            console.log("connected")
+            if (!err){
+                console.log("connected no error")
+                self.connection.query(query, function(error, rows, fields){
+                    if (error){
+                        response = {
+                            serverStatus: 400,
+                            correuEnviat: false,
+                            msg: 'error connection'
+                        };
+                        callback(response);
+                        return; 
+                    }
+                    if (rows.length === 1){
+                        self.emailService.sendEmail(rows[0].Correu,"Emergencia rebuda", Text,callback)
+                    }else{
+                        response = {
+                            serverStatus: 400,   
+                            correuEnviat: false,
+                            msg: 'User invalid'
+                        };
+                        callback(response);
+                    }
+                });
+            }else{
+                response = {
+                        serverStatus: 400, 
+                        correuEnviat: false,
+                        msg: 'error connection'
+                }; 
+                callback(response);
+            }
+        });                   
     }
 
 }
