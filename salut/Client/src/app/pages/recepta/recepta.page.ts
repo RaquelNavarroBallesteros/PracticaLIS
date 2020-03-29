13328da3eb1f49ae94c3addd843cbc35898ef4f9
Guillem <guillem.jardi@e-campus.uab.cat>
2020-03-29 11:35:15 +0200

    Creació del servei Fotoservice natiu, tota la funcionalitat de la foto pasa al servei

--- salut/Client/src/app/pages/recepta/recepta.page.ts
+++ salut/Client/src/app/pages/recepta/recepta.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import {FotoService} from 'src/app/services/foto.service'
 import { File } from "@ionic-native/file/ngx";
 @Component({
   selector: "app-recepta",
@@ -7,30 +7,13 @@ import { File } from "@ionic-native/file/ngx";
   styleUrls: ["./recepta.page.scss"]
 })
 export class ReceptaPage implements OnInit {
-  constructor(private camera: Camera) {}
-  capturedSnapURL: string;
-  cameraOptions: CameraOptions = {
-    quality: 20,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    saveToPhotoAlbum: true
-  };
+  constructor(public fotoService: FotoService) {}
+  
+
+
   ngOnInit() {}
 
   ferFoto() {
-    this.camera.getPicture(this.cameraOptions).then(
-      imageData => {
-        // this.camera.DestinationType.FILE_URI gives file URI saved in local
-        // this.camera.DestinationType.DATA_URL gives base64 URI
-
-        let base64Image = "data:image/jpeg;base64," + imageData;
-        this.capturedSnapURL = base64Image;
-      },
-      err => {
-        console.log(err);
-        // Handle error
-      }
-    );
+    this.fotoService.ferFoto();
   }
 }
--- salut/Client/src/app/services/foto.service.ts
+++ salut/Client/src/app/services/foto.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { File } from "@ionic-native/file/ngx";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FotoService {
+  capturedSnapURL: string;
+  
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    saveToPhotoAlbum: true
+  };
+  constructor(private camera: Camera) { }
+  ferFoto() {
+    this.camera.getPicture(this.cameraOptions).then(
+      imageData => {
+        // this.camera.DestinationType.FILE_URI gives file URI saved in local
+        // this.camera.DestinationType.DATA_URL gives base64 URI
 
-  constructor() { }
+        let base64Image = "data:image/jpeg;base64," + imageData;
+        this.capturedSnapURL = base64Image;
+      },
+      err => {
+        console.log(err);
+        // Handle error
+      }
+    );
+  }
+
 }
