93de1cb1634aae19027ea9e47d23162431ad096a
XaviCoret <xavier.coret@e-campus.uab.cat>
2020-04-12 17:34:21 +0200

    Frontend Informacio

---
+++ salut/Client/src/app/pages/informacio/informacio.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-informacio',
+  templateUrl: './informacio.page.html',
+  styleUrls: ['./informacio.page.scss'],
+})
+export class InformacioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
